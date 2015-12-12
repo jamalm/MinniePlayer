dee18022bea4a83b9eee357ae8f5711e187c2911
Jamal <bluedocjamal@hotmail.co.uk>
2015-12-12 11:10:29 +0000

    First Implementation of My Music app

---
+++ Song.java
@@ -0,0 +1,28 @@
+package com.example.deadmadness.minnieplayer;
+
+/**
+ * Created by deadmadness on 11/12/15.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songId, String songTitle, String songArtist) {
+        id = songId;
+        title = songTitle;
+        artist = songArtist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
