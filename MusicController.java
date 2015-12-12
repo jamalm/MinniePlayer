f92abfe3a99b2756fb46162de0451ec6812fcfec
Jamal <bluedocjamal@hotmail.co.uk>
2015-12-12 11:10:12 +0000

    First Implementation of My Music app

---
+++ MusicController.java
@@ -0,0 +1,17 @@
+package com.example.deadmadness.minnieplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+/**
+ * Created by deadmadness on 11/12/15.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+
+    public void hide()
+    {}
+}
